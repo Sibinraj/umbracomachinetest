323d009e34e60352db5b6d8e71636254b530e699
SibinRaj <sibin.raj@globalmedia.ae>
2018-12-08 22:10:54 +0530

    Completed

---
+++ umbracoMachineTest/App_Code/contactFormSurfaceController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Models;
+//using Umbraco.Web.Mvc;
+using umbraco;
+using System.Text;
+//using System.Net.Mail;
+using System.Configuration;
+using System.Security.Cryptography;
+using System.Net.Mail;
+namespace MyControllers
+{
+    public class contactFormSurfaceController : Umbraco.Web.Mvc.SurfaceController
+    {
+        [ChildActionOnly]
+        public ActionResult ShowForm()
+        {
+            return PartialView("ContactForm", new contactModel());
+        }
+
+        public ActionResult HandleFormPost(contactModel model)
+        {
+            var newComent = Services.ContentService.CreateContent(model.contactName + " " + DateTime.Now.ToString("dd-MM-yyyy HH:mm"), CurrentPage.Id, "listContact");
+            newComent.SetValue("contactName", model.contactName);
+            newComent.SetValue("email", model.email);
+            newComent.SetValue("phone", model.phone);
+            newComent.SetValue("subject", model.subject);
+            newComent.SetValue("message", model.message);
+            var result = Services.ContentService.SaveAndPublishWithStatus(newComent);
+            if (result.Success)
+            {
+                return RedirectToCurrentUmbracoPage();
+            }
+            else
+            {
+                return RedirectToCurrentUmbracoPage();
+            }
+        }
+    }
+}
---
+++ umbracoMachineTest/App_Code/contactModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Umbraco.Core.Models;
+//using Umbraco.Web;
+using Umbraco.Web.Models;
+
+namespace Models
+{
+    public class contactModel
+    {
+        public string contactName { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string subject { get; set; }
+        public string message { get; set; }
+    }
+}
